20f975710fbe3ce050203ef32c46ebd98b327bf1
= <=>
2025-10-16 20:38:46 -0600

    feat(layout): add breadcrumb component and integrate ito admin layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,13 @@
-<x-admin-layout>
+<x-admin-layout :breadcrumbs="[
+    [
+        'name'=>'ITMérida',
+        'href'=> route('admin.dashboard'),
+    ],
+    [
+       'name'=>'DSC',
+        'href'=> route('admin.dashboard'), 
+    ],
+    ['name' => 'Halo'],
+]">
  Hola iris
 </x-admin-layout>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,3 +1,6 @@
+{{--toma los parametros del dash board--}}
+@props(['breadcrumbs'=>[]])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -28,10 +31,13 @@
 
 <div class="p-4 sm:ml-64">
         <!--añadir margen superior-->
-        <div class="mt-14"> 
-             {{$slot}}
+        <div class="mt-14 flex items-center justify-between w-full"> 
+          @include('layouts.includes.admin.breadcrumb')
+
         </div>
 
+      {{$slot}}
+      
     </div>
         @stack('modals')
 
